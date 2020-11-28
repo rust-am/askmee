42c2d292d1e814e507dc789064b3234a4582c659
rustam023 <r.91@mail.ru>
2020-11-28 22:47:54 +0300

    add some models migrates

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,37 @@
 class Question < ApplicationRecord
+
+  belongs_to :user
+
+  validates :text, :user, presence: true
+
+
+
+  #
+  # before_validation :before_validation
+  # after_validation :after_validation
+  #
+  # before_save :before_save
+  # after_save :after_save
+  #
+  # before_create :before_create
+  # after_create :after_create
+  #
+  # before_update :before_update
+  # after_update :after_update
+  #
+  # before_destroy :before_destroy
+  # after_destroy :after_destroy
+  #
+  # private
+  #
+  # # Динамически сгенерируем пару методов для каждого действия, используя
+  # # возможности руби (этот код нужен только для демонстрации валидаций и потом
+  # # мы его удалим).
+  # %w(validation save create update destroy).each do |action|
+  #   %w(before after).each do |time|
+  #     define_method("#{time}_#{action}") do
+  #       puts "8******> #{time} #{action}"
+  #     end
+  #   end
+  # end
 end
