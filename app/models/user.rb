e631b1ed5abf2c2b55b7429dbcf3b6710e9761a0
rustam023 <r.91@mail.ru>
2020-12-07 17:44:29 +0300

    modified question & user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,26 +7,20 @@ class User < ApplicationRecord
 
   has_many :questions
 
-  before_validation :username_to_downcase
+  before_validation :downcase_username, downcase_email
 
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
-  # нашел такое стандартное решение
-  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :email, :username, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
 
-  validates :username, length: {maximum: 40}
-  validates :username, format: {with: /\A[a-zA-Z0-9_]*\z/,
-                                message: "A-Z, a-z, 0-9, _ characters only available."}
+  validates :username, length: {maximum: 40}, format: {with: /\A[a-zA-Z0-9_]*\z/,
+                                                       message: "A-Z, a-z, 0-9, _ characters only available."}
 
   attr_accessor :password
 
-  validates :password, presence:true, on: :create
+  validates :password, confirmation: true, presence: true, on: :create
   # устаревший способ
   # validates_presence_of :password, on: :create
-  validates :password, confirmation: true
-  # устаревший способ
   # validates_confirmation_of :password
-
+  
   before_save :encrypt_password
 
   private
@@ -55,7 +49,11 @@ class User < ApplicationRecord
     end
   end
 
-  def username_to_downcase
-    username.downcase! if username.present?
+  def downcase_username
+    self.username = username.downcase if username.present?
+  end
+
+  def downcase_email
+    self.email = email.downcase if email.present?
   end
 end
