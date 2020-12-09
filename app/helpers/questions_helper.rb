2a857957bf015169f71e9b4dd81632ab68c435b1
rustam023 <r.91@mail.ru>
2020-12-09 10:02:31 +0300

    modified inclination func

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,11 +1,15 @@
 module QuestionsHelper
 
-  def inclination(num, word1, word2, word3)
+  def inclination(num, slon, slona, slonov)
     div10 = num % 10
     div100 = num % 100
-    return word3 if [11, 12, 13, 14].include?(div100)
-    return word1 if div10 == 1
-    return word2 if [2, 3, 4].include?(div10)
-    return word3
+    # исключения - возвращаем форму множественного сущ. для чисел 11, 12, 13, 14
+    return slonov if [11, 12, 13, 14].include?(div100)
+    # сущ. в единственном числе, если число заканчивается на 1(кроме 11, см. выше)
+    return slon if div10 == 1
+    # форма сущ. для чисел заканчивающихся на 2, 3, 4 (кроме 12, 13, 14, см. выше)
+    return slona if [2, 3, 4].include?(div10)
+    # форма множественного числа для всех остальных случаев
+    return slonov
   end
 end
