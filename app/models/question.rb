3899c6d116329a67e02eda5ca6b5ba598c68229a
rustam023 <r.91@mail.ru>
2021-02-04 22:00:53 +0300

    add HashtagQuestion model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :hashtags, through: :hashtag_questions
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
