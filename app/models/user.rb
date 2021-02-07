92decbd70272233ca220c8e028d7df35efa58b3d
rustam023 <r.91@mail.ru>
2021-02-07 21:23:31 +0300

    bang meth bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   end
 
   def self.authenticate(email, password)
-    user = find_by(email: email&.downcase!)
+    user = find_by(email: email&.downcase)
 
     if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
       user
